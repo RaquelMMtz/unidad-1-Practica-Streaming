ded6de27419803f84565e92556df419d61fefb5e
I20050025 <raquel_maya1202@hotmail.com>
2024-09-03 14:07:48 -0600

    Streaming

---
+++ Audio Y video/Audio Y video/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Audio_Y_video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(605, 242);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 280);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(195, 280);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 356);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Audio Y video/Audio Y video/Form1.cs
@@ -0,0 +1,128 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+
+namespace Audio_Y_video
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice videoSource;
+        private WaveInEvent waveIn;
+        private BufferedWaveProvider bufferedWaveProvider;
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            InitializeVideo();
+            InitializeAudio();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeVideo()
+        {
+            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+        }
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+           
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+        private void InitializeAudio()
+        {
+            waveIn = new WaveInEvent();
+
+            // Configura la frecuencia de muestreo y el número de canales
+            waveIn.WaveFormat = new WaveFormat(22050, 1); // Frecuencia de muestreo de 22.05kHz, Mono
+
+            bufferedWaveProvider = new BufferedWaveProvider(waveIn.WaveFormat)
+            {
+                BufferLength = 65536 // Tamaño del búfer en bytes (ej. 64KB)
+            };
+
+            // Configura para descartar datos en exceso cuando el búfer esté lleno
+            bufferedWaveProvider.DiscardOnBufferOverflow = true;
+
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+        }
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            try
+            {
+                // Añadir datos al búfer
+                bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Manejar el error de buffer lleno aquí
+                Console.WriteLine("Error al añadir datos al búfer: " + ex.Message);
+            }
+
+        }
+        private bool isRecordingAudio = false;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null && !videoSource.IsRunning)
+            {
+                videoSource.Start(); // Iniciar la captura de video
+                btnStart.Enabled = false; // Deshabilitar el botón Start
+                btnStop.Enabled = true; // Habilitar el botón Stop
+            }
+
+            // Verificar si el audio ya está grabando usando el indicador
+            if (!isRecordingAudio)
+            {
+                waveIn.StartRecording(); // Iniciar la captura de audio
+                isRecordingAudio = true; // Actualizar el estado de grabación
+                btnStart.Enabled = false; // Deshabilitar el botón Start
+                btnStop.Enabled = true; // Habilitar el botón Stop
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.Stop(); // Detener la captura de video
+            }
+
+            // Detener la captura de audio si está grabando usando el indicador
+            if (isRecordingAudio)
+            {
+                waveIn.StopRecording(); // Detener la captura de audio
+                isRecordingAudio = false; // Actualizar el estado de grabación
+            }
+
+            btnStart.Enabled = true; // Habilitar el botón Start
+            btnStop.Enabled = false; // Deshabilitar el botón Stop
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            if (videoSource.IsRunning)
+                videoSource.Stop();
+            waveIn.Dispose();
+            base.OnClosed(e);
+        }
+        
+
+
+
+
+    }
+}
